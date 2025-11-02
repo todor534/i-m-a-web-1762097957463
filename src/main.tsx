3f3734224d0450bfb5858e790b052db57eaf9f18
todor534 <t.todorov534@gmail.com>
2025-11-02 17:39:20 +0200

    chore: update src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles/global.css';
+
+const rootEl = document.getElementById('root');
+
+if (!rootEl) {
+  throw new Error('Root element not found. Ensure index.html contains <div id="root"></div>.');
+}
+
+createRoot(rootEl).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
